3967c7ca03a4efc6ea413b2d5aa334ab72dae52e
Eric Hebert <ayebear@gmail.com>
2023-11-25 15:47:40 -0500

    Larger tool brush

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,3 @@
 pub const WIDTH: u32 = 800;
 pub const HEIGHT: u32 = 600;
+pub const BRUSH_RADIUS: i32 = 16;
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
-use crate::element::Element;
+use crate::{consts::BRUSH_RADIUS, element::Element};
 use bevy::prelude::*;
 
 #[derive(Resource)]
 pub struct Sandbox {
-    pub size: UVec2,
+    pub size: IVec2,
     pub elements: Vec<Element>,
     pub image_handle: Handle<Image>,
 }
@@ -11,15 +11,21 @@ pub struct Sandbox {
 impl Sandbox {
     pub fn new(width: u32, height: u32, image_handle: Handle<Image>) -> Self {
         Self {
-            size: UVec2::new(width, height),
+            size: IVec2::new(width as i32, height as i32),
             elements: vec![Element::Empty; width as usize * height as usize],
             image_handle,
         }
     }
 
     pub fn draw(&mut self, position: IVec2, element: Element) {
-        let position = position.clamp(IVec2::ZERO, self.size.as_ivec2()).as_uvec2();
-        let i = (position.y * self.size.x + position.x) as usize;
-        self.elements[i] = element;
+        for y in position.y - BRUSH_RADIUS..position.y + BRUSH_RADIUS {
+            for x in position.x - BRUSH_RADIUS..position.x + BRUSH_RADIUS {
+                if x < 0 || x > self.size.x || y < 0 || y > self.size.y {
+                    continue;
+                }
+                let i = (y * self.size.x + x) as usize;
+                self.elements[i] = element;
+            }
+        }
     }
 }

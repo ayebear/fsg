c33669891edd838533c317ac81373de025f1ef69
Eric Hebert <ayebear@gmail.com>
2023-11-25 15:51:14 -0500

    Circular brush

--- src/utils.rs
+++ src/utils.rs
@@ -20,7 +20,12 @@ impl Sandbox {
     pub fn draw(&mut self, position: IVec2, element: Element) {
         for y in position.y - BRUSH_RADIUS..position.y + BRUSH_RADIUS {
             for x in position.x - BRUSH_RADIUS..position.x + BRUSH_RADIUS {
-                if x < 0 || x > self.size.x || y < 0 || y > self.size.y {
+                if x < 0
+                    || x > self.size.x
+                    || y < 0
+                    || y > self.size.y
+                    || position.distance_squared(IVec2::new(x, y)) > BRUSH_RADIUS * BRUSH_RADIUS
+                {
                     continue;
                 }
                 let i = (y * self.size.x + x) as usize;
